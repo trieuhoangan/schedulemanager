f170ee48614263afe4b861e3dbb45fd0f4d274f0
iceiceiceiceiceice <31066905+iceiceiceiceiceice@users.noreply.github.com>
2019-05-12 13:21:21 +0700

    add user

---
+++ user.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM schedulemanager.user
+LIMIT 0, 1000
+
+-- Date: 2019-04-24 12:09
+*/
+INSERT INTO `user` (`id`,`username`,`password`,`role`) VALUES (1,'a','1','ROLE_ADMIN');
+INSERT INTO `user` (`id`,`username`,`password`,`role`) VALUES (2,'admin@gmail.com','12345678','ROLE_ADMIN');
+INSERT INTO `user` (`id`,`username`,`password`,`role`) VALUES (3,'hoang','1','ROLE_ADMIN');
+INSERT INTO `user` (`id`,`username`,`password`,`role`) VALUES (4,'huy','1','ROLE_ADMIN');
